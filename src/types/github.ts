a1d60cf42c61ec85490285d37fb064a1d976f4e7
zhopiich <zhoping9449@gmail.com>
2026-04-30 13:26:41 +0800

    feat(pages): add UserDetailPage for detailed profile viewing

--- src/types/github.ts
+++ src/types/github.ts
@@ -5,6 +5,16 @@ export interface GitHubUser {
   html_url: string
 }
 
+export interface GitHubUserDetail extends GitHubUser {
+  name: string | null
+  bio: string | null
+  public_repos: number
+  followers: number
+  following: number
+  blog: string | null
+  location: string | null
+}
+
 export interface SearchResult {
   total_count: number
   items: GitHubUser[]
