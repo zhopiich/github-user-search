58c90cbbb5bd87cc08edd521ba9591363a69aa2d
zhopiich <zhoping9449@gmail.com>
2026-05-01 09:33:29 +0800

    refactor(state): replace FavoritesProvider with Zustand store

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import { FavoritesProvider } from './context/FavoritesContext'
 import FavoritesPage from './pages/FavoritesPage'
 import SearchPage from './pages/SearchPage'
 import UserDetailPage from './pages/UserDetailPage'
 import './App.css'
 
-function AppContent() {
-  return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<SearchPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-        <Route path="/user/:login" element={<UserDetailPage />} />
-      </Routes>
-    </div>
-  )
-}
-
 export default function App() {
   return (
     <BrowserRouter>
-      <FavoritesProvider>
-        <AppContent />
-      </FavoritesProvider>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<SearchPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="/user/:login" element={<UserDetailPage />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   )
 }
