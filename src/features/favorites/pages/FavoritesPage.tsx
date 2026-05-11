9504190dce4de9e80df8a4092ff2362480bff165
zhopiich <zhoping9449@gmail.com>
2026-05-11 20:58:29 +0800

    refactor(favorites): migrate favorites logic to a feature-based structure

--- src/features/favorites/pages/FavoritesPage.tsx
+++ src/features/favorites/pages/FavoritesPage.tsx
@@ -2,7 +2,7 @@ import type { GitHubUser } from '@/types/github'
 import { useMemo, useState } from 'react'
 import UserCard from '@/components/UserCard'
 import { useFavoritesStore } from '@/store/favoritesStore'
-import FavoriteNoteEditor from './FavoriteNoteEditor'
+import FavoriteNoteEditor from '../components/FavoriteNoteEditor'
 
 interface FavoritesContentProps {
   favorites: GitHubUser[]
